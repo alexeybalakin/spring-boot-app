c16b3837fc25d02ff4771f0caced7c27a67db63d
alexeybalakin <kontrmail@gmail.com>
2018-09-30 18:18:28 +0300

    add upload dir

--- src/main/java/com/ardecs/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/ardecs/springbootapp/SpringBootAppApplication.java
@@ -1,7 +1,10 @@
 package com.ardecs.springbootapp;
 
+import com.ardecs.springbootapp.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
